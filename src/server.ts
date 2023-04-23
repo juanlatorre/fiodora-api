65d398e260eb57a8c0dd6805c93ee709a696959e
Juan Latorre <juanlatorreharcha@gmail.com>
2023-04-23 10:25:11 -0400

    chore: fastify

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,62 @@
-import { createServer } from "node:http";
-import { createYoga } from "graphql-yoga";
-import { ENV } from "./env";
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
 import { schema } from "./schema";
+import { createYoga } from "graphql-yoga";
 
-const yoga = createYoga({
+const app = fastify({ logger: true });
+
+const yoga = createYoga<{
+  req: FastifyRequest;
+  reply: FastifyReply;
+}>({
   schema,
+  logging: {
+    debug: (...args) => args.forEach((arg) => app.log.debug(arg)),
+    info: (...args) => args.forEach((arg) => app.log.info(arg)),
+    warn: (...args) => args.forEach((arg) => app.log.warn(arg)),
+    error: (...args) => args.forEach((arg) => app.log.error(arg)),
+  },
 });
 
-const server = createServer(yoga);
+app.route({
+  url: "/",
+  method: ["GET", "POST", "OPTIONS"],
+  handler: async (req, reply) => {
+    const response = await yoga.handleNodeRequest(req, {
+      req,
+      reply,
+    });
 
-server.listen(ENV.PORT, () => {
-  console.log(`\nServer running http://0.0.0.0:${ENV.PORT}/graphql`);
+    reply.status(response.status);
+    reply.send(response.body);
+
+    return reply;
+  },
+});
+
+app.route({
+  url: "/graphql",
+  method: ["GET", "POST", "OPTIONS"],
+  handler: async (req, reply) => {
+    const response = await yoga.handleNodeRequest(req, {
+      req,
+      reply,
+    });
+    response.headers.forEach((value, key) => {
+      reply.header(key, value);
+    });
+
+    reply.status(response.status);
+
+    reply.send(response.body);
+
+    return reply;
+  },
+});
+
+app.listen({ host: "0.0.0.0", port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`\nServer listening at ${address}`);
 });
